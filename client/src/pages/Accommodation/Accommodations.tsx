e0882119dee4cf89a385858cdfb3a83e3994537e
miloszKubiak <milypan69@gmail.com>
2023-02-26 11:39:06 +0100

    listing accommodations on the user page

--- client/src/pages/Accommodation/Accommodations.tsx
+++ client/src/pages/Accommodation/Accommodations.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export type TAccommodation = {
+  _id?: string;
   title: string;
   address: string;
   description: string;
@@ -18,7 +19,9 @@ export type TAccommodation = {
 };
 
 export const Accommodations = () => {
-  const [accommodations, setAccommodations] = useState<TAccommodation | []>([]);
+  const [accommodations, setAccommodations] = useState<TAccommodation[] | []>(
+    []
+  );
 
   useEffect(() => {
     axios.get("/accommodations").then(({ data }) => setAccommodations(data));
@@ -32,7 +35,29 @@ export const Accommodations = () => {
           <GoPlus />
           Add new
         </Link>
-        <p>accommodations</p>
+        <div className="mt-8 flex flex-col gap-4">
+          {accommodations.length > 0 &&
+            accommodations.map((accommodation) => (
+              <Link
+                to={`/account/accommodations/${accommodation._id}`}
+                className="p-4 flex gap-4 bg-gray-100 rounded-2xl cursor-pointer"
+                key={accommodation._id}
+              >
+                <div className="w-32 h-32 bg-gray-300 grow shrink-0">
+                  {accommodation.photos!.length > 0 && (
+                    <img
+                      src={accommodation.photos![0]}
+                      alt="main photo of the accommodation"
+                    />
+                  )}
+                </div>
+                <div className="grow-0 shrink">
+                  <h2 className="text-xl font-bold">{accommodation.title}</h2>
+                  <p>{accommodation.description}</p>
+                </div>
+              </Link>
+            ))}
+        </div>
       </div>
     </div>
   );
