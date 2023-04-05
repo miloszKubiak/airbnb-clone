78d8f19db27e4bdd15696fbb226a9c003d43fa92
miloszKubiak <milypan69@gmail.com>
2023-04-05 16:02:43 +0200

    add pagination on backend to the user accommodations

--- server/src/controllers/accommodationController.ts
+++ server/src/controllers/accommodationController.ts
@@ -38,8 +38,23 @@ export const addNewAccommodation = async (req: Request, res: Response) => {
 };
 
 export const getUserAccommodations = async (req: Request, res: Response) => {
-  const accommodations = await Accommodation.find({ owner: req.cookies.owner });
-  res.status(200).json({ count: accommodations.length, accommodations });
+  const queryObject = {
+    owner: req.cookies.owner,
+  };
+  let result = Accommodation.find(queryObject);
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 2;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const accommodations = await result;
+
+  const totalAccommodations = await Accommodation.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalAccommodations / limit);
+
+  res.status(200).json({ accommodations, totalAccommodations, numOfPages });
 };
 
 export const getSingleAccommodation = async (req: Request, res: Response) => {
