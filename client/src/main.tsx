513e2f52b38f8597621d175ed04ffe84f50f825c
miloszKubiak <milypan69@gmail.com>
2023-06-22 15:36:30 +0200

    fix refetching in pagination

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -24,7 +24,14 @@ import { ReviewsContextProvider } from "./context/ReviewsContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 300000,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
